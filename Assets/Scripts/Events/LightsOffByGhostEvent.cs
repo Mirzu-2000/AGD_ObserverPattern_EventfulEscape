4fa44e4e147788fec5819534c9499daf76ed5974
Mirza_4565 <mirzaalishaik2000@gmail.com>
2025-07-12 12:48:45 +0530

    Add ghost-triggered lights off event logic
    Introduces serialized fields for required keys and ghost laugh sound in LightsOffByGhostEvent. Implements OnTriggerEnter to trigger the event and play sound when the player has the required keys, and disables the collider after activation. Also updates prefab to match new field names.

--- Assets/Scripts/Events/LightsOffByGhostEvent.cs
+++ Assets/Scripts/Events/LightsOffByGhostEvent.cs
@@ -2,5 +2,17 @@ using UnityEngine;
 
 public class LightsOffByGhostEvent : MonoBehaviour
 {
+    [SerializeField] private int keysRequriedToTrigger;
+    [SerializeField] private SoundType gostLaugh;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerView>() != null && keysRequriedToTrigger == GameService.Instance.GetPlayerController().KeysEquipped)
+        {
+            EventService.Instance.OnLightsOffByGhostEvent.InvokeEvent();
+            GameService.Instance.GetSoundView().PlaySoundEffects(gostLaugh);
+           GetComponent<Collider>().enabled = false; // Disable the collider to prevent multiple triggers
+        }
+    }
 
 }
